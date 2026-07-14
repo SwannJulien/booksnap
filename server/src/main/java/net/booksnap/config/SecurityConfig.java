3511f163a02a03e16a2c7322e6a300b737544662
Swann Julien <swann.jn@gmail.com>
2026-07-14 16:19:10 +0200

    feat: configure CORS in java app

--- server/src/main/java/net/booksnap/config/SecurityConfig.java
+++ server/src/main/java/net/booksnap/config/SecurityConfig.java
@@ -11,6 +11,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
--- server/src/main/java/net/booksnap/config/WebConfiguration.java
+++ server/src/main/java/net/booksnap/config/WebConfiguration.java
@@ -13,8 +13,8 @@ public class WebConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:8000")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
